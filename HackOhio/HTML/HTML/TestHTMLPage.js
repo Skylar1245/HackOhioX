3c2c9bca208510e1ed995f3af59341be618c39cc
azdoc21 <100966372+azdoc21@users.noreply.github.com>
2022-10-08 18:02:04 -0400

    carousel

--- HackOhio/HTML/HTML/TestHTMLPage.js
+++ HackOhio/HTML/HTML/TestHTMLPage.js
@@ -1,1 +1,13 @@
+function displayNextImage() {
+    console.log('working')
+    if(document.getElementById('img').src == 'https://www.mathsisfun.com/data/images/graph-gen.svg') {
+        document.getElementById('img').src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Polynomial_of_degree_three.svg/1200px-Polynomial_of_degree_three.svg.png'
+    } else {
+        document.getElementById('img').src = 'https://www.mathsisfun.com/data/images/graph-gen.svg'
+    }
+}
+
+movingblock = document.getElementsByClassName('graphs')
+console.log(movingblock)
+movingblock[0].addEventListener("animationiteration", displayNextImage);
 
