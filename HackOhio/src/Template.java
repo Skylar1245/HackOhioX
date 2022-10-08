ca457de4d6464bba42f69ab2104972792652d215
kateG03 <kategoertz@outlook.com>
2022-10-08 10:36:52 -0400

    test.

--- HackOhio/src/Template.java
+++ HackOhio/src/Template.java
@@ -29,7 +29,11 @@ public final class Template {
     public static void main(String[] args) {
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
+        //test
 
+        
+        
+        
         /*
          * Close input and output streams
          */
