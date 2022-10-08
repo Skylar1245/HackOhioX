795de4d2264903b0df078792a1e274a9b67810e7
Skylar Stephens <myloslayer1245@gmail.com>
2022-10-08 16:38:44 -0400

    Data interpreting methods

--- HackOhio/src/Main.java
+++ HackOhio/src/Main.java
@@ -69,7 +69,11 @@ public final class Main {
      *            line with data
      * @return correct List
      */
-    public static List<String> getRecordFromLine(String line) {
+    public static List<String> getDataFromLine(String line) {
+        /*
+         * Try to open a scanner based on the current line, if you can add all
+         * data seperated by commas to a list of strings
+         */
         List<String> values = new ArrayList<String>();
         try (Scanner rowScanner = new Scanner(line)) {
             rowScanner.useDelimiter(",");
@@ -88,15 +92,91 @@ public final class Main {
      * @return a list<list<string>>
      */
     public static List<List<String>> createDataList(String fileName) {
+        /*
+         * Try to make a scanner of the filename, if you can add the data from
+         * the line to the list through getREcordFromLine
+         */
         List<List<String>> records = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File(fileName));) {
             while (scanner.hasNextLine()) {
-                records.add(getRecordFromLine(scanner.nextLine()));
+                records.add(getDataFromLine(scanner.nextLine()));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return records;
+    }
+
+    /**
+     * Converts a List<List<String>> to a String[][].
+     *
+     * @param l
+     *            a List<List<String>>
+     * @return a String[][]
+     */
+    public static String[][] createMatrix(List<List<String>> l) {
+        /*
+         * for all list elements add a String[] based on the contents
+         */
+        int rows = l.size();
+        int columns = l.get(0).size();
+
+        String[][] matrix = new String[rows][columns];
+
+        for (int i = 0; i < columns; i++) {
+            for (int k = 0; k < rows; k++) {
+                matrix[k][i] = l.get(k).get(i);
+            }
+        }
+        return matrix;
+    }
+
+    /**
+     * Checks if a string can be parsed to a double.
+     *
+     * @param s
+     *            String to be checked
+     * @return true or false
+     */
+    public static boolean canParseToDouble(String s) {
+        /**
+         * Check if s can be parsed into a double
+         */
+        boolean canParse = true;
+        try {
+            Double.parseDouble(s);
+        } catch (NumberFormatException e) {
+            canParse = false;
+        }
+        return canParse;
+    }
+
+    /**
+     * Averages the correct values in a matrix line.
+     *
+     * @param matrix
+     *            matrix to work with
+     * @param pos
+     *            small matrix pos
+     * @return the average of the line
+     */
+    public static double getAverage(String[][] matrix, int pos) {
+        /*
+         * Initial values
+         */
+        double average = 0.0;
+        double pop = 0.0;
+        /*
+         * for all values in the matrix, if they are valid numbers at to average
+         * can count the data population
+         */
+        for (int i = 0; i < matrix.length; i++) {
+            if (canParseToDouble(matrix[i][pos])) {
+                average += Double.parseDouble(matrix[i][pos]);
+                pop++;
+            }
+        }
+        return (average / pop);
     }
 
     /**
@@ -118,9 +198,15 @@ public final class Main {
         String weatherFile = "data/Weather Data.csv";
 
         List<List<String>> dormData = createDataList(dormFile);
-        //List<List<String>> nonDormData = createDataList(nonDormFile);
-        //List<List<String>> weatherData = createDataList(weatherFile);
+        List<List<String>> nonDormData = createDataList(nonDormFile);
+        List<List<String>> weatherData = createDataList(weatherFile);
 
+        String[][] dormMatrix = createMatrix(dormData);
+        //String[][] nonDormMatrix = createMatrix(nonDormData);
+        //String[][] weatherMatrix = createMatrix(weatherData);
+
+        out.println("Averaging " + dormMatrix[0][13] + ".");
+        out.println(getAverage(dormMatrix, 13));
         /*
          * Webpage stuff.
          */
@@ -132,10 +218,11 @@ public final class Main {
 
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
-        //out.println(Math.floor(timeElapsed) + " seconds");
+        out.println("Ran in: " + Math.floor(timeElapsed) + " seconds");
         /*
          * Close input and output streams
          */
+
         in.close();
         out.close();
     }
